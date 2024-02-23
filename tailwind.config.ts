9077c9b6dc4f3cc61969e3275057861ea79eb544
Velina Kennedy <velinagutierrez@Velinas-iMac.local>
2024-02-23 17:06:35 -0500

    setup navbar components without functionality

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'purple-text': '#424286',
+        'purple-primary': '#6161D6',
+        'purple-secondary': '#CCCCFA'
+      },
     },
   },
   plugins: [],
