661e8a74f2374827d8823ed720d8124ce9593d77
Velina Kennedy <velinagutierrez@Velinas-iMac.local>
2024-03-19 18:50:35 -0400

    setup redux

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+'use client'
+
+import { useGetMarketDataQuery } from "./redux/features/marketDataSlice";
+
 export default function Home() {
+
+  const {data, isLoading} = useGetMarketDataQuery("");
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <main>
-      
+         <p>data: {JSON.stringify(data)}</p>
     </main>
   );
 }
