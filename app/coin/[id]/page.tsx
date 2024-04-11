471404a10d15a2876595ccff55d6c89ac878d4e9
Velina Kennedy <velinagutierrez@Velinas-iMac.local>
2024-04-10 22:27:18 -0400

    created api call to fetch coin information to display on the coin page

--- app/coin/[id]/page.tsx
+++ app/coin/[id]/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+import { useGetCoinQuery } from "../../redux/features/coinInfoSlice";
+
 const CoinInfo = ({params: {id}}: {params: {id: string}}) => {
+  const {currentData, isSuccess}= useGetCoinQuery(id);
+  const hasData = currentData && isSuccess;
+  
   return (
-    <h1 className="text-white">{id}</h1>
+    <div>
+      {hasData && (
+        <div>
+          <h1 className="text-white">{currentData.name}</h1>
+          <p>{currentData.description.en}</p>
+        </div>
+      )}
+    </div>
   );
 };
 export default CoinInfo;
--- app/redux/store.ts
+++ app/redux/store.ts
@@ -3,15 +3,17 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 import { marketSlice } from "./features/marketDataSlice";
 import currencyReducer from "./features/currencySlice";
 import { coinListSlice } from "./features/coinListSlice";
+import { coinInfoSlice } from "./features/coinInfoSlice";
 
 export const store = configureStore({
     reducer: {
         [marketSlice.reducerPath]: marketSlice.reducer,
         [coinListSlice.reducerPath]: coinListSlice.reducer,
+        [coinInfoSlice.reducerPath]: coinInfoSlice.reducer,
         currency: currencyReducer
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(marketSlice.middleware, coinListSlice.middleware),
+        getDefaultMiddleware().concat(marketSlice.middleware, coinListSlice.middleware, coinInfoSlice.middleware),
   });
 
 setupListeners(store.dispatch);
