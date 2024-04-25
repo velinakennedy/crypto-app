b9de80f1d227dc75d5939d88aef1fe3ef80805bb
Velina Kennedy <velinagutierrez@Velinas-iMac.local>
2024-04-25 16:45:45 -0400

    changed grid layout in coin page and switched button to link in coin link component

--- app/coin/[id]/page.tsx
+++ app/coin/[id]/page.tsx
@@ -13,63 +13,154 @@ const CoinInfo = ({ params: { id } }: { params: { id: string } }) => {
   const currency = useSelector((state: RootState) => state.currency.value);
   const { currentData, isSuccess } = useGetCoinQuery(id);
   const hasData = currentData && isSuccess;
-  // if (hasData) console.log(currentData);
+
   return (
     <div>
       {hasData && (
-        <div className="grid grid-cols-2 mx-20 mt-7 gap-7 text-purple-text dark:text-white">
-          <div className="grid grid-cols-2 gap-7">
-            <div className="flex flex-col gap-4">
-              <div className="flex flex-col items-center justify-center h-full p-10 rounded-lg dark:bg-purple-market bg-purple-secondary gap-7">
-                <Image
-                  loader={() => currentData.image.large}
-                  src={currentData.image.large}
-                  width={80}
-                  height={80}
-                  alt="coin logo"
-                />
-                <h1>{currentData.name}</h1>
+        <div className="grid mx-20 grid-rows-[20rem_10rem_20rem] mt-7 gap-7 text-purple-text dark:text-white">
+          <div className="grid grid-cols-2 row-start-1 gap-7">
+            <div className="grid grid-cols-2 gap-7">
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col items-center justify-center h-full p-10 rounded-lg dark:bg-purple-market bg-purple-secondary gap-7">
+                  <Image
+                    loader={() => currentData.image.large}
+                    src={currentData.image.large}
+                    width={80}
+                    height={80}
+                    alt="coin logo"
+                  />
+                  <h1>{currentData.name}</h1>
+                </div>
+                <CoinLink link={currentData.links.homepage[0]} />
               </div>
-              <CoinLink link={currentData.links.homepage[0]}/>
+              <div className="p-10 rounded-lg dark:bg-purple-market bg-purple-secondary">
+                <div className="flex flex-wrap justify-center gap-3">
+                  <span className="text-3xl font-semibold">
+                    {formatCurrency(
+                      currentData.market_data.current_price[currency],
+                      6,
+                      false,
+                      currency
+                    )}
+                  </span>
+                  <span className="text-xl font-semibold">
+                    <PriceChange
+                      value={
+                        currentData.market_data.price_change_percentage_24h
+                      }
+                    />
+                  </span>
+                </div>
+                <div className="flex flex-col items-center gap-3 mt-10">
+                  <div>
+                    <div className="flex flex-wrap items-center gap-3">
+                      <IoMdArrowDropup className="text-4xl text-green-500 dark:text-teal-positive" />
+                      <h3 className="font-semibold dark:font-normal">
+                        All time high:
+                      </h3>{" "}
+                      <span className="text-xl font-bold">
+                        {formatCurrency(
+                          currentData.market_data.ath[currency],
+                          6,
+                          false,
+                          currency
+                        )}
+                      </span>
+                    </div>
+                    <div className="text-sm dark:font-thin">
+                      {formatDate(currentData.market_data.ath_date[currency])}
+                    </div>
+                  </div>
+                  <div>
+                    <div className="flex flex-wrap items-center gap-3">
+                      <IoMdArrowDropdown className="text-4xl text-red-negative" />
+                      <h3 className="font-semibold dark:font-normal">
+                        All time low:
+                      </h3>{" "}
+                      <div className="text-xl font-bold">
+                        {formatCurrency(
+                          currentData.market_data.atl[currency],
+                          6,
+                          false,
+                          currency
+                        )}
+                      </div>
+                    </div>
+                    <div className="text-sm dark:font-thin">
+                      {formatDate(currentData.market_data.atl_date[currency])}
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="p-10 rounded-lg dark:bg-purple-market bg-purple-secondary">
-              <div className="flex flex-wrap justify-center gap-3">
-                <span className="text-3xl font-semibold">{formatCurrency(currentData.market_data.current_price[currency], 6, false, currency)}</span>
-                <span className="text-xl font-semibold"><PriceChange value={currentData.market_data.price_change_percentage_24h} /></span>
-              </div> 
-              <div className="flex flex-col items-center gap-3 mt-10">
-                <div>
-                <div className="flex flex-wrap items-center gap-3"><span><IoMdArrowDropup className="text-4xl text-green-500 dark:text-teal-positive"/></span>All time high: <span className="text-xl font-semibold">{formatCurrency(currentData.market_data.ath[currency], 6, false, currency)}</span></div>
-                <div className="text-sm font-thin">{formatDate(currentData.market_data.ath_date[currency])}</div>
-                </div>
-                <div>
-                <div className="flex flex-wrap items-center gap-3"><span><IoMdArrowDropdown className="text-4xl text-red-negative"/></span>All time low: <span className="text-xl font-semibold">{formatCurrency(currentData.market_data.atl[currency], 6, false, currency)}</span></div>
-                <div className="text-sm font-thin">{formatDate(currentData.market_data.atl_date[currency])}</div>
-                </div>
+            <div className="grid grid-cols-2 p-10 rounded-lg dark:bg-purple-market bg-purple-secondary">
+              <div className="flex flex-col items-center justify-center gap-7">
+                <h2 className="dark:font-thin">Market Cap</h2>
+                <p className="font-semibold">
+                  {formatCurrency(
+                    currentData.market_data.market_cap[currency],
+                    5,
+                    true,
+                    currency
+                  )}
+                </p>
+                <h2 className="dark:font-thin">Fully Diluted Valuation</h2>
+                <p className="font-semibold">
+                  {formatCurrency(
+                    currentData.market_data.fully_diluted_valuation[currency],
+                    5,
+                    true,
+                    currency
+                  )}
+                </p>
+                <h2 className="dark:font-thin">Volume/Market</h2>
+                <p className="font-semibold">
+                  {formatCurrency(
+                    currentData.market_data.market_cap[currency] /
+                      currentData.market_data.total_volume[currency],
+                    5,
+                    true,
+                    currency
+                  )}
+                </p>
+              </div>
+              <div className="flex flex-col items-center justify-center gap-7">
+                <h2 className="dark:font-thin">Total Volume</h2>
+                <p className="font-semibold">
+                  {formatCurrency(
+                    currentData.market_data.total_volume[currency],
+                    5,
+                    true,
+                    currency
+                  )}
+                </p>
+                <h2 className="dark:font-thin">Circulating Supply</h2>
+                <p className="font-semibold">
+                  {formatCurrency(
+                    currentData.market_data.circulating_supply,
+                    5,
+                    true
+                  )}{" "}
+                  {currentData.symbol.toUpperCase()}
+                </p>
+                <h2 className="dark:font-thin">Max Supply</h2>
+                <p className="font-semibold">
+                  {formatCurrency(currentData.market_data.max_supply, 5, true)}{" "}
+                  {currentData.symbol.toUpperCase()}
+                </p>
               </div>
             </div>
           </div>
-          <div className="grid grid-cols-2 p-10 rounded-lg dark:bg-purple-market bg-purple-secondary">
-            <div className="flex flex-col items-center justify-center gap-7">
-              <h2 className="font-thin">Market Cap</h2>
-              <p className="font-semibold">{formatCurrency(currentData.market_data.market_cap[currency], 5, true, currency)}</p>
-              <h2 className="font-thin">Fully Diluted Valuation</h2>
-              <p className="font-semibold">{formatCurrency(currentData.market_data.fully_diluted_valuation[currency], 5, true, currency)}</p>
-              <h2 className="font-thin">Volume/Market</h2>
-              <p className="font-semibold">{formatCurrency(currentData.market_data.market_cap[currency]/currentData.market_data.total_volume[currency], 5, true, currency)}</p>
+          <h1 className="flex items-end text-2xl font-bold">Description </h1>
+          <div className="grid grid-cols-2 row-start-3 gap-7">
+            <div
+              dangerouslySetInnerHTML={{ __html: currentData.description.en }}
+            ></div>
+            <div className="flex flex-col gap-7">
+              <CoinLink link={currentData.links.blockchain_site[1]} />
+              <CoinLink link={currentData.links.blockchain_site[2]} />
+              <CoinLink link={currentData.links.blockchain_site[3]} />
             </div>
-            <div className="flex flex-col items-center justify-center gap-7">
-              <h2 className="font-thin">Total Volume</h2>
-              <p className="font-semibold">{formatCurrency(currentData.market_data.total_volume[currency], 5, true, currency)}</p>
-              <h2 className="font-thin">Circulating Supply</h2>
-              <p className="font-semibold">{formatCurrency(currentData.market_data.circulating_supply, 5, true)} {currentData.symbol.toUpperCase()}</p>
-              <h2 className="font-thin">Max Supply</h2>
-              <p className="font-semibold">{formatCurrency(currentData.market_data.max_supply, 5, true)} {currentData.symbol.toUpperCase()}</p>
-            </div>
-          </div>
-          <div className="pt-28">
-            <h1 className="mb-10 text-2xl font-bold">Description </h1>
-          <div dangerouslySetInnerHTML={{ __html: currentData.description.en }}></div>
           </div>
         </div>
       )}
--- app/components/CoinLink.tsx
+++ app/components/CoinLink.tsx
@@ -1,22 +1,23 @@
 import copyLink from "@/utils/copyLink";
 import { IoCopyOutline } from "react-icons/io5";
 import { FaLink } from "react-icons/fa6";
+import Link from "next/link";
 
-const CoinLink = ({link}: {link: string}) => {
+const CoinLink = ({ link }: { link: string }) => {
   return (
-    <div className="flex items-center justify-center rounded-lg gap-7 p-7 dark:bg-purple-market bg-purple-secondary">
-    <button
-      onClick={() =>
-        window.open(link, "_blank")
-      }
-    >
-      <FaLink />
-    </button>
-    {link}
-    <button onClick={() => copyLink(link)}>
-      <IoCopyOutline />
-    </button>
-  </div>
+    <div>
+      {link && (
+        <div className="flex items-center justify-center rounded-lg gap-7 p-7 dark:bg-purple-market bg-purple-secondary">
+          <Link href={link} target="_blank">
+            <FaLink />
+          </Link>
+          <p className="overflow-clip">{link}</p>
+          <button onClick={() => copyLink(link)}>
+            <IoCopyOutline />
+          </button>
+        </div>
+      )}
+    </div>
   );
 };
 export default CoinLink;
