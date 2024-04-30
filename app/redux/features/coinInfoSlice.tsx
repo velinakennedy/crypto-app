5392a27aa440a96d3e3e6500d17b93baa7e00632
Velina Kennedy <velinagutierrez@Velinas-iMac.local>
2024-04-29 21:49:11 -0400

    fixed cors error

--- app/redux/features/coinInfoSlice.tsx
+++ app/redux/features/coinInfoSlice.tsx
@@ -2,10 +2,10 @@ import {fetchBaseQuery, createApi} from "@reduxjs/toolkit/query/react";
 
 export const coinInfoSlice = createApi({
     reducerPath: "coinInfo",
-    baseQuery: fetchBaseQuery({baseUrl: "https://api.coingecko.com/"}),
+    baseQuery: fetchBaseQuery({baseUrl: "https://api.coingecko.com/", mode: "cors"}),
     endpoints: (builder) => ({
         getCoin: builder.query({
-            query: (id: string) => `/api/v3/coins/${id}?localization=false&tickers=false&market_data=true&community_data=true&developer_data=false&sparkline=false`
+            query: (id: string) => `/api/v3/coins/${id}?x_cg_demo_api_key=${process.env.NEXT_PUBLIC_API_KEY}&localization=false&tickers=false&market_data=true&community_data=true&developer_data=false&sparkline=false`
         })
     })
 });
